270188ff1e6de7c950ba8029c66a7ab9eaaf57f6
Chriss5-2 <christiangiovannixd@gmail.com>
2026-04-16 23:43:53 -0500

    Chapter 13: Interact With Other Modules

---
+++ tutorials/state_account/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Real State Account',
+    'version': '1.0',
+    'category': 'Real State',
+    'summary': 'Real State Account Management',
+    'depends': ['state', 'account'],
+}
---
+++ tutorials/state_account/models/estate_property.py
@@ -0,0 +1,25 @@
+from odoo import models, Command
+
+class InheritedModel(models.Model):
+    _inherit = "estate.property"
+
+    def action_set_sold(self):
+        for property_record in self:
+            self.env["account.move"].create(
+            {
+                "partner_id": property_record.buyer_id.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    Command.create({
+                        "name": f"Comision venta: {property_record.name}",
+                        "quantity": 1,
+                        "price_unit": property_record.selling_price * 0.06,
+                    }),
+                    Command.create({
+                        "name": "Gastos administrativos",
+                        "quantity": 1,
+                        "price_unit": 100.0,
+                    }),
+                ]
+            })
+        return super().action_set_sold()
