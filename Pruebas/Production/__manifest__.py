04ffd30eba5e2006ddb5ef000d5a0afad81532fe
Chriss5-2 <christiangiovannixd@gmail.com>
2026-04-25 17:59:00 -0500

    Creando menú general del modulo Producción según el archivo

---
+++ Pruebas/Production/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Menus Production',
+    'version': '1.0',
+    'category': 'Production Testing',
+    'summary': 'Menus Production Management',
+    'depends': ['base'],
+    'data': [
+        'views/principal_menu.xml',
+        'views/produccion_menu.xml',
+        'views/planeamiento_menu.xml',
+        'views/control_calidad_menu.xml',
+        'views/costos_menu.xml',
+        'views/mantenimiento_menu.xml',
+        'views/utilidades_menu.xml',
+    ]
+}
